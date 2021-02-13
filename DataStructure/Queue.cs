1eaae93191a7ae640239cb11716e4d2e1fa98abc
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-13 22:53:54 +0530

    [MaheshKangude] ADDED: AbilityTo CreateQueue UsingLinkedList

---
+++ DataStructure/Queue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    public class Queue
+    {
+        LinkedList linkedList;
+
+        public Queue()
+        {
+            linkedList = new LinkedList();
+        }
+
+        public void Enqueue(Node node)
+        {
+            linkedList.AppendNode(node);
+        }
+
+        public void DisplayQueue()
+        {
+            Console.WriteLine("Queue:");
+            linkedList.DisplayLinkedList();
+        }
+
+        public Node Top()
+        {
+            return linkedList.Head;
+        }
+    }
+}
---
+++ NUnitTestProject/QueueUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructure;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    class QueueUnitTest
+    {
+        Queue queue;
+        [SetUp]
+        public void Setup()
+        {
+            queue = new Queue();
+        }
+
+        [Test]
+        public void GivenNodes_WhenAddedToStack_ShouldPassStackTest()
+        {
+            Node node1 = new Node(56);
+            Node node2 = new Node(30);
+            Node node3 = new Node(70);
+
+            queue.Enqueue(node1);
+            queue.Enqueue(node2);
+            queue.Enqueue(node3);
+
+            Node Top = queue.Top();
+
+            Assert.AreEqual(node1, Top);
+        }
+    }
+}
