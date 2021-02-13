1b3206abb5bc33ad72be4e9cff61ad3d378bb6bc
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-13 17:22:00 +0530

    [MaheshKangude] REFACTPRED: SimpleLinkedList TestCase

--- NUnitTestProject/UnitTest.cs
+++ NUnitTestProject/UnitTest.cs
@@ -5,20 +5,24 @@ namespace NUnitTestProject
 {
     public class Tests
     {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
         [Test]
-        public void GivenNodes_WhenAdded_ShouldPassLinkedListTest()
+        public void GivenNodes_WhenAddedToSimpleList_ShouldPassLinkedListTest()
         {
             Node node1 = new Node(56);
             Node node2 = new Node(30);
             Node node3 = new Node(70);
 
-            LinkedList List = new LinkedList();
-            List.AddNode(node1);
-            List.AddNode(node2);
-            List.AddNode(node3);
-            bool result = List.Head.Equals(node3) &&
-                          List.Head.next.Equals(node2) &&
-                          List.Tail.Equals(node1);
+            node1.next = node2;
+            node2.next = node3;
+
+            bool result = node1.next.Equals(node2) &&
+                          node2.next.Equals(node3) &&
+                          node3.next == null;
             Assert.IsTrue(result);
         }
     }
