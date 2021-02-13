0f9155993c4b2e343e0937322db5e40282f26551
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-13 12:42:52 +0530

    [MaheshKangude] ADDED: SimpleLinkedList

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    class LinkedList
+    {
+        internal Node Head;
+        internal void Add(int Data)
+        {
+            Node node = new Node(Data);
+            if (this.Head == null)
+                this.Head = node;
+            else
+            {
+                Node temp = Head;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = node;
+            }
+        }
+
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+                Console.WriteLine("Linked list:");
+            while(temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)                  
+                    Console.Write("->");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -6,7 +6,11 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.Display();
         }
     }
 }
