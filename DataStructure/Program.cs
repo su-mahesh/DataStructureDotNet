72e1fd02bd87bca6e3d8cb5869533823acb33432
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-13 22:06:07 +0530

    [MaheshKangude] REFACTORED: removed redundant code

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -6,22 +6,6 @@ namespace DataStructure
     {
         static void Main()
         {
-            LinkedList List = new LinkedList();
-            Node node1 = new Node(1197);
-            Node node2 = new Node(180);
-            Node node3 = new Node(170);
-            Node node4 = new Node(160);
-            Node node5 = new Node(150);
-            Node node6 = new Node(140);
-
-            List.AddNodeInSort(node1);
-            List.AddNodeInSort(node2);
-            List.AddNodeInSort(node3);
-            List.AddNodeInSort(node4);
-            List.AddNodeInSort(node5);
-            List.AddNodeInSort(node6);
-
-            List.Display();
         }
     }
 }
